7d02effa940b6219da1659d539455eda46491264
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2022-02-20 16:32:54 +0530

    Update urls.py

--- crypto/urls.py
+++ crypto/urls.py
@@ -6,6 +6,6 @@ from .views import MyHistoryAPI, CryptoInfoAPI, CryptoBuyAPI, CryptoSellAPI
 urlpatterns = [
     path("info", CryptoInfoAPI.as_view(), name='info'),
     path("history", MyHistoryAPI.as_view(), name='history'),
-    path("buy", CryptoBuyAPI.as_view(), name='info'),
-    path("sell", CryptoSellAPI.as_view(), name='info'),
+    path("buy", CryptoBuyAPI.as_view(), name='cpt_buy'),
+    path("sell", CryptoSellAPI.as_view(), name='cpt_sell'),
 ]
