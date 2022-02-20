082c438e8a351f2ba92fd4419f13da46f545072d
Harishankar Kumar <31770598+hari01584@users.noreply.github.com>
2022-02-20 16:48:48 +0530

    Stocks over

--- stocks/models.py
+++ stocks/models.py
@@ -7,58 +7,6 @@ import yfinance as yf
 
 User = get_user_model()
 
-# class StocksBalance(models.Model):
-#     code = models.CharField(max_length=5)
-#     amount = models.FloatField()
-
-# class StocksWallet(models.Model):
-#     user = AutoOneToOneField(User, related_name='stocks', on_delete=models.CASCADE)
-#     balance = models.ForeignKey(StocksBalance, null=True, on_delete=models.CASCADE)
-
-#     def operate(self, stock_id, amount):
-#         pass
-
-#     def get_all_ledger(self):
-#         data = {}
-
-#         ledger = list(StocksWalletLedger.objects.filter(stockwallet=self))
-#         if(len(ledger) <= 0):
-#             data['code'] = -1
-#             data['message'] = "no ledger record for your stocks"
-#             return data
-
-#         data['code'] = 1
-#         data['data'] = []
-#         for d in ledger:
-#             data['data'].append(d.as_dict())
-#         return data
-
-
-
-# class ActionType(Enum):
-#     BUY="buy"
-#     SELL="sell"
-
-# class StocksWalletLedger(models.Model):
-#     stockwallet = models.ForeignKey(StocksWallet, related_name='ledger', on_delete=models.CASCADE)
-#     action_type = models.CharField(max_length=5, choices=[(tag, tag.value) for tag in ActionType])
-#     targetStock = models.CharField(max_length=5,)
-#     pricePerStock = models.FloatField()
-#     quantity = models.PositiveIntegerField()
-
-#     created = models.DateTimeField(auto_now_add=True)
-
-#     def as_dict(self):
-#         return {
-#             "id": self.id,
-#             "action": self.action_type,
-#             "targetStock": self.targetStock,
-#             "price": self.pricePerStock,
-#             "quantity": self.quantity,
-#             "created": self.created,
-#         }
-
-
 class StockWallet(models.Model):
     user = AutoOneToOneField(User, related_name='stocks', on_delete=models.CASCADE)
 
@@ -193,6 +141,7 @@ class StockWalletLedger(models.Model):
     @staticmethod
     def create_ledger_record(wallet, action, targetStock, pricePerc, quan):
         r = StockWalletLedger(stockwallet=wallet, action_type=action.value, targetStock=targetStock, pricePerStock=pricePerc, quantity=quan)
+        r.save()
         return r
 
 
